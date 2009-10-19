f698491c4dc4917104364e581d830ce4c209e4af
Tomas Drencak <tomas@drencak.com>
2009-10-19 19:47:01 +0200

    bocny panel pri studentovi

--- src/schools/students/urls.py
+++ src/schools/students/urls.py
@@ -3,9 +3,9 @@ from django.views.generic.create_update import create_object, update_object
 from schools.search.views import object_list
 from schools.students.models import Student
 
-urlpatterns = patterns('',
+urlpatterns = patterns('schools.students.views',
     url(r'student/create/$', create_object, {'model':Student}, name='students_student_create'),
-    url(r'student/(?P<object_id>\d+)/$', update_object, {'model':Student}, name='students_student_update'),
+    url(r'student/(?P<object_id>\d+)/$', 'update_student', name='students_student_update'),
     url(r'student/$', object_list, {'queryset':Student.objects.all(), 'search_fields':['last_name__contains', 'first_name__contains']}, name='students_student_list'),
     
 )
--- src/schools/students/views.py
+++ src/schools/students/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from generic_views.views.create_update import update_object
+from django.shortcuts import get_object_or_404
+from schools.students.models import Student
+
+def update_student(request, object_id):
+    return update_object(request, object_id=object_id, model=Student, extra_context={'student':get_object_or_404(Student, pk=object_id)})
