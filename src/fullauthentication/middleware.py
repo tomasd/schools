f59dd47362523980d50ffa4f6bd304279575aef5
Tomas Drencak <tomas@drencak.com>
2010-01-22 17:38:37 +0100

    fix testov

--- src/fullauthentication/middleware.py
+++ src/fullauthentication/middleware.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import login, logout
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.views.static import serve
-from django.utils.http import urlencode, urlquote
+from django.utils.http import urlquote
 class FullAuthenticationMiddleware(object):
     def process_request(self, request):
+        # fix for tests
+        if request.META['SERVER_NAME'] == 'testserver':
+            return
         if request.path == reverse(login) or request.path == reverse(logout) or 'media' in request.path:
             return
         
