54dd2c6fd9d6690ec9ec5928fed7eb856e7c8a16
Tomas Drencak <tomas@drencak.com>
2010-01-22 22:11:17 +0100

    acl pre firmy

--- src/schools/companies/urls.py
+++ src/schools/companies/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.create_update import create_object
 from schools.companies.models import Company
-from schools.search.views import object_list
-from generic_views.views.delete import delete_object
 
 urlpatterns = patterns('schools.companies.views',
-    url(r'company/create/$', create_object, {'model':Company}, name='companies_company_create'),
+    url(r'company/create/$', 'company_create', {'model':Company}, name='companies_company_create'),
     url(r'company/(?P<object_id>\d+)/$', 'company_update', {'model':Company}, name='companies_company_update'),
     url(r'company/(?P<object_id>\d+)/students/$', 'company_students', name='companies_company_students'),
-    url(r'company/(?P<object_id>\d+)/delete/$', delete_object, {'model':Company, 'post_delete_redirect':'companies_company_list'}, name='companies_company_delete'),
-    url(r'company/$', object_list, {'queryset':Company.objects.all(), 'search_fields':['name__contains']}, name='companies_company_list'),
+    url(r'company/(?P<object_id>\d+)/delete/$', 'company_delete', {'model':Company, 'post_delete_redirect':'companies_company_list'}, name='companies_company_delete'),
+    url(r'company/$', 'company_list', {'queryset':Company.objects.all(), 'search_fields':['name__contains']}, name='companies_company_list'),
 )
--- src/schools/companies/views.py
+++ src/schools/companies/views.py
@@ -1,12 +1,28 @@
 # Create your views here.
-from generic_views.views.create_update import update_object
+from generic_views.views.create_update import update_object, create_object
 from django.shortcuts import get_object_or_404
 from schools.companies.models import Company
 from schools.search.views import object_list
+from schools import permission_required
+from generic_views.views.delete import delete_object
+
+@permission_required('companies.add_company')
+def company_create(*args, **kwargs):
+    return create_object(*args, **kwargs)
+
+@permission_required('companies.change_company')
 def company_update(request, *args, **kwargs):
     kwargs['extra_context'] = kwargs.get('extra_context', {})
     kwargs['extra_context']['company'] = get_object_or_404(Company, pk=kwargs['object_id'])
     return update_object(request, *args, **kwargs)
+
+@permission_required('companies.delete_company')
+def company_delete(*args, **kwargs):
+    return delete_object(*args, **kwargs)
+
+@permission_required('companies.add_company', 'companies.change_company', 'companies.delete_company')
+def company_list(*args, **kwargs):
+    return object_list(*args, **kwargs)
 
 def company_students(request, object_id):
     company = get_object_or_404(Company, pk=object_id)
