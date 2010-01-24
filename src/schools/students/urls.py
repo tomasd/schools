e1491ead32149cf3cc7271092770ee081974c828
Tomas Drencak <tomas@drencak.com>
2010-01-24 13:58:43 +0100

    student acl

--- src/schools/students/urls.py
+++ src/schools/students/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls.defaults import patterns, url
-from generic_views.views.create_update import create_object
-from generic_views.views.delete import delete_object
-from schools.search.views import object_list
 from schools.students.models import Student
 
 urlpatterns = patterns('schools.students.views',
-    url(r'student/create/$', create_object, {'model':Student}, name='students_student_create'),
-    url(r'student/(?P<object_id>\d+)/$', 'update_student', name='students_student_update'),
+    url(r'student/create/$', 'student_create', {'model':Student}, name='students_student_create'),
+    url(r'student/(?P<object_id>\d+)/$', 'student_update', name='students_student_update'),
     url(r'student/(?P<object_id>\d+)/courses/$', 'student_courses', name='students_student_courses'),
-    url(r'student/(?P<object_id>\d+)/delete/$', delete_object, {'model':Student, 'post_delete_redirect':'students_student_list'}, name='students_student_delete'),
-    url(r'student/$', object_list, {'queryset':Student.objects.all(), 'search_fields':['last_name__contains', 'first_name__contains']}, name='students_student_list'),
+    url(r'student/(?P<object_id>\d+)/delete/$', 'student_delete', {'model':Student, 'post_delete_redirect':'students_student_list'}, name='students_student_delete'),
+    url(r'student/$', 'student_list', {'queryset':Student.objects.all(), 'search_fields':['last_name__contains', 'first_name__contains']}, name='students_student_list'),
     
 )
--- src/schools/students/views.py
+++ src/schools/students/views.py
@@ -1,13 +1,28 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404
-from generic_views.views.create_update import update_object
+from generic_views.views.create_update import update_object, create_object
 from schools.search.views import object_list
 from schools.students.models import Student
+from generic_views.views.delete import delete_object
+from schools import permission_required
 
-def update_student(request, object_id):
+@permission_required('students.add_student')
+def student_create(*args, **kwargs):
+    return create_object(*args, **kwargs)
+
+@permission_required('students.change_student')
+def student_update(request, object_id):
     return update_object(request, object_id=object_id, model=Student, extra_context={'student':get_object_or_404(Student, pk=object_id)})
+
+@permission_required('students.delete_student')
+def student_delete(*args, **kwargs):
+    return delete_object(*args, **kwargs)
+
+@permission_required('students.delete_student', 'students.change_student', 'students.add_student')
+def student_list(*args, **kwargs):
+    return object_list(*args, **kwargs)
 
 def student_courses(request, object_id):
     student = get_object_or_404(Student, pk=object_id)
     queryset = student.coursemember_set.all()
-    return object_list(request, ('course__name__contains',) ,queryset=queryset, template_name='students/student_courses_list.html', extra_context={'student':student})
+    return object_list(request, ('course__name__contains',) , queryset=queryset, template_name='students/student_courses_list.html', extra_context={'student':student})
