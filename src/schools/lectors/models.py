633228d0d36df0d8914a0d673a12f23897e9c088
Tomas Drencak <tomas@drencak.com>
2010-02-04 22:43:51 +0100

    prve meno lektora ako prve

--- src/schools/lectors/models.py
+++ src/schools/lectors/models.py
@@ -33,8 +33,8 @@ class LectorManager(models.Manager):
 # Create your models here.
 class Lector(models.Model):
     objects = LectorManager()
-    last_name = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     
     title = models.CharField(max_length=10, null=True, blank=True)
     
