0fe9c8df9dc48dee646349a8d8c8009f7bd1c5c2
tomas <tomas@tomas-laptop.(none)>
2010-04-13 19:47:40 +0200

    fakturacia knih u platitela

--- src/schools/companybook/views.py
+++ src/schools/companybook/views.py
@@ -1,11 +1,18 @@
 # Create your views here.
+from django.http import Http404
 from django.shortcuts import get_list_or_404
 from schools.companies.models import Company
-from schools.reports.views import invoice
-from django.http import Http404
+from schools.reports.views import invoice, book_invoice_dict
 
 def companybook(request):
     companies = Company.objects.filter(users=request.user)
     if not companies:
         raise Http404()
-    return invoice(request, companies=companies, template='companybook/companybook.html', extra_context={'companies':companies})
+    book_context = book_invoice_dict(request, companies, companies)
+    book_context = dict([('book_%s' % key, value) for key, value in book_context.items()])
+    extra_context = {'companies':companies}
+    extra_context.update(book_context)
+    return invoice(request, 
+                   companies=companies, 
+                   template='companybook/companybook.html', 
+                   extra_context=extra_context)
